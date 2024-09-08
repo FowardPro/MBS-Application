6c7f048641dca859b780c24b8c25cc8b70963f6c
yinhla <makamuyinhla2016@gmail.com>
2024-09-08 20:49:21 +0200

    created conn script and skeleton of the project

---
+++ src/conn.php
@@ -0,0 +1,9 @@
+<?php 
+$servername = "pdb1058.awardspace.net";
+$username = "4089676_ict";
+$password = "Password@123";
+$dbname = "4089676_ict";
+
+$con = mysqli_connect($servername, $username,$password, $dbname);
+
+?>

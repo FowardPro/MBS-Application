0de3ec027b46781e6e8de556c2e337b1ff857375
yinhla <makamuyinhla2016@gmail.com>
2024-09-08 20:56:38 +0200

    changed link

--- src/conn.php
+++ src/conn.php
@@ -1,8 +1,8 @@
 <?php 
 $servername = "pdb1058.awardspace.net";
-$username = "4089676_ict";
+$username = "4445469_outsidesite";
 $password = "Password@123";
-$dbname = "4089676_ict";
+$dbname = "4445469_outsidesite";
 
 $con = mysqli_connect($servername, $username,$password, $dbname);
 
